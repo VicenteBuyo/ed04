166e06b38cbc1d8d1e4ff57e67e29e77bb1d5399
VicenteBuyo <3170415@alu.murciaeduca.es>
2025-03-16 19:22:51 +0100

    Refactorización: Creado el método initAgenda para inicializar la agenda

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,7 @@ public class Main {
         // Crear una nueva agenda.
         Agenda agendaContactos = new Agenda();
 
-        // Añadir contactos a la agenda.
-        agendaContactos.addContact("John Doe", "1234567890");
-        agendaContactos.addContact("Jane Doe", "9876543210");
-        agendaContactos.addContact("Mary Jane", "1122334455");
+        initAgenda(agendaContactos);
 
         // Imprimir todos los contactos de la agenda.
         System.out.println("Todos los contactos: ");
@@ -16,18 +13,29 @@ public class Main {
             System.out.println(c.getName() + " -> " + c.getPhones());
         }
 
-        // Añadir un nuevo teléfono a un contacto existente.
-        agendaContactos.addContact("John Doe", "6543219876");
 
-        // Eliminar un contacto de la agenda.
-        agendaContactos.removeContact("Jane Doe");
 
-        // Modificar el teléfono de un contacto existente en la agenda.
-        agendaContactos.modifyPhoneNumber("John Doe", "1234567890", "6666666666");
+
+
+
 
         System.out.println("\nDespués de añadir, eliminar y modificar contactos: ");
         for (Contacto c : agendaContactos.getContacts()) {
             System.out.println(c.getName() + " -> " + c.getPhones());
         }
+    }
+
+    private static void initAgenda(Agenda agendaContactos) {
+        // Añadir contactos a la agenda.
+
+        agendaContactos.addContact("John Doe", "1234567890");
+        agendaContactos.addContact("Jane Doe", "9876543210");
+        agendaContactos.addContact("Mary Jane", "1122334455");
+        // Añadir un nuevo teléfono a un contacto existente.
+        agendaContactos.addContact("John Doe", "6543219876");
+        // Eliminar un contacto de la agenda.
+        agendaContactos.removeContact("Jane Doe");
+        // Modificar el teléfono de un contacto existente en la agenda.
+        agendaContactos.modifyPhoneNumber("John Doe", "1234567890", "6666666666");
     }
 }
